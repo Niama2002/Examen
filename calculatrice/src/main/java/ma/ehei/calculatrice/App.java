1a5297b376e9c4db04c3c1e2e6e920b7cb6571bb
Niama <niamachibani123@gmail.com>
2024-01-04 11:14:38 +0100

    modification des fichiers java

--- calculatrice/src/main/java/ma/ehei/calculatrice/App.java
+++ calculatrice/src/main/java/ma/ehei/calculatrice/App.java
@@ -2,6 +2,10 @@ package ma.ehei.calculatrice;
 
 public class App {
 	public static void main(String[] args) {
+		System.out.println(Calculatrice.multiplication(2, 5));
 		System.out.println(Calculatrice.add(5, 5));
+		
+		
+	
 	}
 }
